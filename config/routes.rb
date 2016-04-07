6b033ff4461d8334dc5ceed6fc18407541923fd9
Kimberly Oleiro <koleiro@gmail.com>
2016-04-07 18:39:43 -0400

    Created picture model in Database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'cars' => 'cars#index'
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
